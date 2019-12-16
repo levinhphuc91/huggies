76e57a21b6e4ad305138ce7cd75bef585b772e73
Le Phuc <malik@aleph-labs.com>
2019-12-16 12:42:30 +0800

    Make it better

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
-var cacheName = 'huggies-pwa';
-var filesToCache = ['/', '/index.html', '/css/style.css', '/js/main.js'];
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js'
+];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
-    }),
+    })
   );
 });
 
@@ -15,6 +20,6 @@ self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
-    }),
+    })
   );
 });
